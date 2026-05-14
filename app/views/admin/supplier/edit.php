ffca9e20a554d0fe127b75629bde62f9a00ae3df
Hanip <haniframdan22@gmail.com>
2026-05-14 15:42:06 +0700

    supplier

--- app/views/admin/supplier/edit.php
+++ app/views/admin/supplier/edit.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Barang - Laboratorium Kewirausahaan</title>
+    <title>Edit Supplier - Laboratorium Kewirausahaan</title>
 
 </head>
 <body>
