fe5505819460273e731503b66e830788149011bf
rizkanurulaulia12-png <rizkanurulaulia12@gmail.com>
2026-05-15 16:43:39 +0700

    Update pagination.php
    pagination.php progres

---
+++ app/views/components/pagination.php
@@ -0,0 +1,7 @@
+<div class="pagination">
+    <a href="#" class="prev">&laquo;</a>
+    <a href="#" class="active">1</a>
+    <a href="#">2</a>
+    <a href="#">3</a>
+    <a href="#" class="next">&raquo;</a>
+</div>
